3286377ad1d36e35cc36331cee3aa656f57bf705
KaranSinghBhandari0 <kb632126@gmail.com>
2024-12-23 00:56:15 +0530

    added new feature (clear chat & forgot password)

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ import { AuthContext } from "./context/AuthContext";
 export default function App() {
   const {user, isAuthenticated, isCheckingAuth, theme, connectSocket} = useContext(AuthContext);
 
+  console.log(import.meta.env.VITE_BACKEND_URL)
+
   // auth check
   useEffect(()=> {
     isAuthenticated();
