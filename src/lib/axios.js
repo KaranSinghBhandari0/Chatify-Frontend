9bf0de4bf06933ac6be7b69c743dc0370445c14b
Karan-Singh-Bhandari-19 <karanbhandari037@gmail.com>
2025-01-14 12:31:02 +0530

    updated backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://chatifyy.up.railway.app",
+    baseURL: "https://chatify-b3y1.onrender.com",
     // baseURL: "http://localhost:3000",
     withCredentials: true,
 });
